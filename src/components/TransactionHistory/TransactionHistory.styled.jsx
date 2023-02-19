78f44760a959ff87e72e3cb218235ad5f393c8a7
ADIKryvonos <108137330+ADIKryvonos@users.noreply.github.com>
2023-02-19 17:12:32 +0200

    fix

--- src/components/TransactionHistory/TransactionHistory.styled.jsx
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -10,13 +10,15 @@ export const Tr = styled.tr``;
 
 export const Th = styled.th`
   border: 1px solid grey;
-  padding: 10px;
+  padding: 5px;
   background-color: aquamarine;
+  font-size: 16px;
 `;
 
 export const Tbody = styled.tbody``;
 
 export const Td = styled.td`
-  padding: 10px;
+  padding: 5px;
   border: 1px solid grey;
+  font-size: 16px;
 `;
